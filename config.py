39a55ccbbd781b8151ff652890cb7dd2e55913b9
mvu <114735662+epsipa2@users.noreply.github.com>
2022-10-19 10:02:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ydl_opts = {
 ydl = YoutubeDL(ydl_opts)
 links=[]
 finalurl=""
-STREAM=os.environ.get("STREAM_URL", "http://node-25.zeno.fm/kezsc0y2wwzuv?listening-from-radio-garden=1622271954020&rj-ttl=5&rj-tok=AAABec5bAE4Aj31dmRAEFgcbvw")
+STREAM=os.environ.get("STREAM_URL", "https://www.liveradio.ie/stations/star-radio-tamil")
 regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
 match = re.match(regex,STREAM)
 if match:
@@ -24,19 +24,19 @@ else:
 class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    API_ID = int(os.environ.get("API_ID", '10670890'))
+    CHAT = int(os.environ.get("CHAT", "-1001768257900"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001768257900")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "Y")
-    DURATION_LIMIT=int(os.environ.get("DUR", 15))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "SESSION")
+    DURATION_LIMIT=int(os.environ.get("DUR", 1500))
+    API_HASH = os.environ.get("API_HASH", "b8c18624a9a4b397e9989c30904de9d2")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5483191761:AAFBHL5pIvS-rZHLskHuk3SgvLY6o0bN5I4") 
+    SESSION = os.environ.get("SESSION_STRING", "BABy3cy3eKp-UkHtUEsQujJsAtnJTTIk1E5DTCFEI10WW_WPOax1H7S43Eqs-SfIo2Dez_iCFDA62GyqjP_P1mbLJdnQIXnxUI11RZ4bimilVb0lNcA-EWV8skTa_Sy3pR3eVWJe91jG1_3zQzCZwBQdF64LlHJkMlNN3AQteyksdczVGJRPU9yd_AnN__YP63nWNi159Pz2PTaMTWY82gpS9GuHDHhZbPt3i5ddey6BiJRLibiJeFF96r8yDvlTTD3m4KclaQcadAicAsPaflQhmsAU6p3kHw-szUpD7GTfYFwUh2bhS7ZYoTN4-kSgZTVkpSuXbKZYKg-M3Ks0lUEPdW1H7gA")
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     playlist=[]
     msg = {}
